27b85805a74d0e947058a616cb4bf0af157ec839
bin2598 <65968419+bin2598@users.noreply.github.com>
2021-01-28 20:52:23 +0530

    Update settings.py

--- doctorAppointment/settings.py
+++ doctorAppointment/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n5t*kop+i!$87#f1a^=rlh+#9k@+n8dd*00zsn-8f(tty#%g&5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
